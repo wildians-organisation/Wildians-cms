285bff7f5a0d4e2ff22551eba1a79404b4b55c2d
tanguypouriel <pourieltanguy@gmail.com>
2025-12-26 13:13:41 +0100

    Switch to directus

--- src/app/(payload)/admin/importMap.js
+++
@@ -1,7 +0,0 @@
-import { default as default_068bbee23a0108eac4b528109f4eb5e1 } from '../../../components/MarkdownField.tsx'
-import { default as default_fc4d7c2d399921eebb3e1dfda643880d } from '../../../components/AnswersFieldComponent'
-
-export const importMap = {
-  "/components/MarkdownField.tsx#default": default_068bbee23a0108eac4b528109f4eb5e1,
-  "/components/AnswersFieldComponent#default": default_fc4d7c2d399921eebb3e1dfda643880d
-}
